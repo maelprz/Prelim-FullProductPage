35626ab4da5b02a9706f2ad937fc93f4fd8d05ec
Mel Sephen A. Perez <mel.perez.5203@gmail.com>
2025-09-28 19:07:16 +0800

    Initial commit: added header image, details section, reply button

---
+++ android/app/src/main/kotlin/com/example/fullproductpage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fullproductpage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProductPage(),
+    );
+  }
+}
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // HEADER IMAGE 
+          SizedBox(
+            width: double.infinity,
+            height: 410,
+            child: Transform.scale(
+              scale: 1.25, // zoom in
+              alignment: Alignment.center, 
+              child: Image.asset(
+                'assets/headerImage.png',
+                fit: BoxFit.cover,
+                alignment: const Alignment(0, -1.05), // Positioning headerImage
+              ),
+            ),
+          ),
+          
+          // SCROLLABLE DETAILS BELOW
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 70),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // ROW 1: Price + Reply
+                  Row(
+                    children: [
+                      // Price
+                      Text(
+                        "\$17.00",
+                        style: GoogleFonts.raleway(
+                          fontSize: 32,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+
+                      const Spacer(), // push button to the very right
+
+                      // Reply button
+                      Row(
+                        children: [
+                          const SizedBox(width: 15), // spacing if needed
+                          Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFFFEBEB),
+                              shape: BoxShape.circle,
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: Transform(
+                              alignment: Alignment.center,
+                              transform: Matrix4.rotationY(3.14159), // flip horizontally
+                              child: Transform.scale(
+                              scale: 1.4,
+                                child: const Icon(
+                                  Icons.reply,
+                                  size: 23,
+                                  color: Color(0xFFB5A2A2),
+                                ),
+                              ),
+                            ),
+                          ), 
+                        ],
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  // ROW 2: Description
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam arcu mauris, scelerisque eu mauris id, pretium pulvinar sapien.",
+                    style: GoogleFonts.nunitoSans(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
